581c3df118e8b6381d2bbd17db772bd17bbb4e56
x <karolina_bp@hotmail.com>
2022-05-18 14:39:58 -0300

    Imprimindo genero e nome do genero do arquivo xml
    Fizemos a primeira consulta usando linq para acessar um arquivo xml

--- AluraTunes/LinqToXml2/Program.cs
+++ AluraTunes/LinqToXml2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Xml.Linq;
+using System.Linq;
+
+//Para acessar um arquivo xml temos que acessar uma biblioteca própria do NET
+XElement root = XElement.Load(@"C:\projetos\AluraTunes\AluraTunes\LinqToXml2\Data\AluraTunes.xml");
+
+//Definir consulta linq que acesse o arquivo
+
+var queryXML = 
+    from g in root.Element("Generos").Elements("Genero")
+    select g;
+
+foreach(var genero in queryXML)
+{
+    Console.WriteLine("{0}\t{1}", genero.Element("GeneroId").Value, genero.Element("Nome").Value);
+}
