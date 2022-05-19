e8c55641ff57c3c37d862c95a57b9d3d02ccbc20
x <karolina_bp@hotmail.com>
2022-05-19 17:18:05 -0300

    Join de genero com faixa de musica imprimindo so os 10 primeiros

--- AluraTunes/LinqToEntities/Program.cs
+++ AluraTunes/LinqToEntities/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LinqToEntities.Models;
+using Microsoft.EntityFrameworkCore;
 
 using (var contexto = new AluraTunesDbContext())
 {
@@ -9,7 +10,26 @@ using (var contexto = new AluraTunesDbContext())
     //imprimir no console
     foreach (var genero in query)
     {
-        Console.WriteLine("{0}\t{1}",genero.GeneroId, genero.Nome) ;
+        Console.WriteLine("{0}\t{1}", genero.GeneroId, genero.Nome);
+    }
+
+    Console.WriteLine();
+
+    //Join de genero e musica
+    var faixaEGenero = from g in contexto.Generos
+                       join f in contexto.Faixas
+                        on g.GeneroId equals f.GeneroId
+                       select new { f, g };
+
+    faixaEGenero = faixaEGenero.Take(10);
+
+    //Console.WriteLine(faixaEGenero.ToQueryString());
+
+    Console.WriteLine(faixaEGenero.ToList());
+
+    foreach (var item in faixaEGenero)
+    {
+        Console.WriteLine("{0}\t{1}", item.f.Nome, item.g.Nome);
     }
 
     Console.ReadKey();
